61e7bb3081b7d5b647802206eb393b50ca5862b2
Zaynah <zaynah.sadiq@hotmail.com>
2023-06-13 15:37:15 +0100

    attempted to fix bugs

--- src/slaps/components/SlapForm.js
+++ src/slaps/components/SlapForm.js
@@ -1,33 +1,32 @@
 import { useState } from "react";
-import Picker from '@emoji-mart/react';
-import data from '@emoji-mart/data';
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
 
-const SlapForm = ({ }) => {
+const SlapForm = ({ user }) => {
+  const [isPickerVisible, setPickerVisible] = useState(false);
+  const [currentEmoji, setCurrentEmoji] = useState(null);
 
-    const [isPickerVisible,setPickerVisible] = useState(false);
-    const [currentEmoji, setCurrentEmoji] = useState(null);
+  return (
+    <div className="emoji-picker">
+      <h1>{currentEmoji || "mood:"}</h1>
+      <button
+        className="emoji"
+        onClick={() => setPickerVisible(!isPickerVisible)}
+      >
+        mood
+      </button>
+      <div className={isPickerVisible ? "d-block" : "d-none"}>
+        <Picker
+          data={data}
+          previewPosition="none"
+          onEmojiSelect={(e) => {
+            setCurrentEmoji(e.native);
+            setPickerVisible(!isPickerVisible);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
-
-
-
-
-    return ( 
-        <div className="emoji-picker">
-            <h1>{currentEmoji || 'mood:'}</h1>
-            <button className="emoji"
-                onClick={() => setPickerVisible(!isPickerVisible)}
-                >
-                mood
-            </button>
-            <div className={isPickerVisible ? 'd-block' : 'd-none'}></div>
-            <Picker data={data}
-                previewPosition="none"
-                onEmojiSelect={(e) => {
-                    setCurrentEmoji(e.native);
-                    setPickerVisible(!isPickerVisible);
-                }}/>
-        </div>
-     );
-}
- 
 export default SlapForm;
