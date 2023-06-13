221fdf8b10d7fafcb002b0a401f36621b61a6f0d
Rada Kanchananupradit <rada.kanchan@gmail.com>
2023-06-13 16:11:51 +0100

    updated emoji

--- src/slaps/components/SlapForm.js
+++ src/slaps/components/SlapForm.js
@@ -3,25 +3,29 @@ import Picker from "@emoji-mart/react";
 import data from "@emoji-mart/data";
 
 const SlapForm = ({ user }) => {
+
   const [isPickerVisible, setPickerVisible] = useState(false);
   const [currentEmoji, setCurrentEmoji] = useState(null);
+
+  const handleTogglePicker = () => setPickerVisible(!isPickerVisible);
 
   return (
     <div className="emoji-picker">
       <h1>{currentEmoji || "mood:"}</h1>
       <button
         className="emoji"
-        onClick={() => setPickerVisible(!isPickerVisible)}
+        onClick={handleTogglePicker}
       >
         mood
       </button>
+
       <div className={isPickerVisible ? "d-block" : "d-none"}>
         <Picker
           data={data}
           previewPosition="none"
           onEmojiSelect={(e) => {
             setCurrentEmoji(e.native);
-            setPickerVisible(!isPickerVisible);
+            // setPickerVisible(!isPickerVisible);
           }}
         />
       </div>
