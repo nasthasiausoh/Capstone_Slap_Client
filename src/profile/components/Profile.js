162c33a0e45c98e5013b8cf1922bb879fd7a0158
Nasthasia Usoh <nasthasia@hotmail.co.uk>
2023-06-14 20:32:40 +0100

    added follow and unfollow functionality

--- src/profile/components/Profile.js
+++ src/profile/components/Profile.js
@@ -1,37 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import '../ProfileStyles.css';
 import ProfileSlapList from './ProfileSlapList';
+import FollowButton from '../../follow/FollowButton';
 
-const Profile = ({user, loggedInUser}) => {
+const Profile = ({ user, loggedInUser }) => {
+  const isCurrentUser = loggedInUser && loggedInUser.id === user.id;
+  const initialFollowing = loggedInUser && loggedInUser.following.some(followedUser => followedUser.id === user.id);
 
   const userSlapComponents = user.slaps.map((userSlap) => {
     return (
-        <ProfileSlapList userSlap = {userSlap}
-         user={user} loggedInUser={loggedInUser}  />
+      <ProfileSlapList
+        userSlap={userSlap}
+        user={loggedInUser.id === user.id ? loggedInUser : user}
+      />
     );
-})
+  });
 
-    return (
-      <div>
-        <section className='profile-bio-component'>
-            <div className='profile-details'>
-                <div className='profile-slap-picture'>
-                   <img src={user.profilePicture} alt='user-profile-picture' width={300}/>
-                </div>
-                <div className='profile-username-follow-bio'>
-                  <h1>{user.username}</h1>
-                  <button id='follow-button'>Following</button>
-                  <p id='user-bio'>"{user.bio}" <button className='bio-button'> Update Bio</button></p>
-                </div>
-            </div>
-        </section>
-
-        <div>
-           {userSlapComponents}
+  return (
+    <div>
+      <section className='bio-component'>
+        <div className='profile-details'>
+          <div className='bio-profile-picture'>
+            <img src={user.profilePicture} alt='user-profile-picture' width={300} />
+          </div>
+          <div className='username-follow-bio'>
+            <h1>{user.username}</h1>
+            {isCurrentUser ? (
+              <button id='follow-button'>Following</button>
+            ) : (
+              <FollowButton loggedInUser={loggedInUser} user={user} initialFollowing={initialFollowing} />
+            )}
+            <p id='user-bio'>
+              "{user.bio}" <button className='bio-button'>Update Bio</button>
+            </p>
+          </div>
         </div>
+      </section>
 
-        </div>
-      );
-    };
+      <div>{userSlapComponents}</div>
+    </div>
+  );
+};
 
 export default Profile;
--- src/timeline/components/Timeline.js
+++ src/timeline/components/Timeline.js
@@ -1,41 +1,42 @@
-import React, { useState } from "react";
-import Slap from "../../slaps/components/Slap";
+import React, { useState } from 'react';
+import Slap from '../../slaps/components/Slap';
 
-const Timeline = ({
-  listOfSlaps,
-  setListOfSlap,
-  listOfUsers,
-  setListOfUsers,
-  loggedInUser
-}) => {
+const Timeline = ({ listOfSlaps, setListOfSlap, listOfUsers, setListOfUsers, loggedInUser }) => {
   const [showOnlyFollowing, setShowOnlyFollowing] = useState(false);
 
   const handleFilterByFollowing = () => {
     setShowOnlyFollowing(!showOnlyFollowing);
   };
 
-  console.log(loggedInUser);
-
   const slapComponents = listOfSlaps
-  .filter((slap) => !showOnlyFollowing || slap.user.followers.map(follower => follower.id).includes(loggedInUser.id))
-  .map((slap) => {
-      return <Slap
+    .filter(slap => (showOnlyFollowing ? loggedInUser.following.some(user => user.id === slap.user.id) : true))
+    .map(slap => (
+      <Slap
+        key={slap.id}
         slap={slap}
         listOfSlaps={listOfSlaps}
         setListOfSlap={setListOfSlap}
-      />;
-    });
+      />
+    ));
 
-  return (        
-      <section className="tl-section">
-      {loggedInUser ? <h1><i>{loggedInUser.username}</i> 's Timeline </h1> : <h1>Timeline</h1> }
-        {loggedInUser ? <button onClick={handleFilterByFollowing}>
-          {showOnlyFollowing ? "Show All Slaps" : "Filter by Friends"}
-        </button> : <h3> General Feed</h3> }
-        <div>{slapComponents}</div>
-      </section>
-
+  return (
+    <section className="tl-section">
+      {loggedInUser ? (
+        <h1>
+          <i>{loggedInUser.username}</i>'s Timeline
+        </h1>
+      ) : (
+        <h1>Timeline</h1>
+      )}
+      {loggedInUser && loggedInUser.following.length > 0 && (
+        <button onClick={handleFilterByFollowing}>
+          {showOnlyFollowing ? 'Show All Slaps' : 'Filter by Friends'}
+        </button>
+      )}
+      <div>{slapComponents}</div>
+    </section>
   );
 };
 
 export default Timeline;
+
