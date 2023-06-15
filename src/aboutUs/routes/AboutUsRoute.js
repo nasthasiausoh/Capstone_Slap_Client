3538025f7b5b018980b552142caa333cb29916cf
Zaynah <zaynah.sadiq@hotmail.com>
2023-06-15 18:19:37 +0100

    added styling

--- src/aboutUs/routes/AboutUsRoute.js
+++ src/aboutUs/routes/AboutUsRoute.js
@@ -7,7 +7,7 @@ const AboutUsRoute = () => {
   return (
     <div>
         <Navbar />
-        <HeroImage heading='ABOUT US.' text='The Slap is a social media platform designed to allow users to express their thoughts in a fun and concise manner. Browse through The Slap Feed, connect with friends and strangers, and join The Slap Nation.
+        <HeroImage heading='ABOUT US' text='The Slap is a social media platform designed to allow users to express their thoughts in a fun and concise manner. Browse through The Slap Feed, connect with friends and strangers, and join The Slap Nation.
 '/>
         {/* HeroImage is where we would insert our avatars */}
         <AboutUs />
