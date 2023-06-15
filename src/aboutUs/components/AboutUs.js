7ed5db89fc0679418aef7d1ace4977ec7f3ce601
Zaynah <zaynah.sadiq@hotmail.com>
2023-06-15 10:46:20 +0100

    added avatars and comments in about us

--- src/aboutUs/components/AboutUs.js
+++ src/aboutUs/components/AboutUs.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../AboutUsStyles.css'
 import zaynahAvatar from './avatar-assets/zaynah-avatar.png'
+import radaAvatar from './avatar-assets/rada-avatar.png'
+import natashaAvatar from './avatar-assets/natasha-avatar.png'
+import subrinaAvatar from './avatar-assets/subrina-avatar.png'
 const AboutUs = () => {
 
   return (
@@ -8,37 +11,41 @@ const AboutUs = () => {
         <div className='card-container'>
             <div className='card'>
                 <h3> - Natasha-</h3>
+                <img src = {natashaAvatar}  alt = "cartoon rendition of Natasha"/>
                 <span className='team-member'> </span>
                 <p className='person'> Natasha</p>
+                <p>Fatal flaw: works too much</p>
                 <p>- abc -</p>
+                <p>- abc -</p>
+                <p>- abc -</p>
+             </div>
+             <div className='card'>
+                <h3> - Zaynah- </h3>
+                <img src = {zaynahAvatar}  alt = "cartoon rendition of Zaynah"/>
+                <span className='team-member'> </span>
+                <p className='person'> Zaynah</p>
+                <p>Fatal flaw: has Hayfever</p>
                 <p>- abc -</p>
                 <p>- abc -</p>
                 <p>- abc -</p>
              </div>
             <div className='card'>
                 <h3> - Rada- </h3>
+                <img src = {radaAvatar}  alt = "cartoon rendition of Rada"/>
                 <span className='team-member'> </span>
                 <p className='person'> Rada</p>
-                <p>- abc -</p>
-                <p>- abc -</p>
-                <p>- abc -</p>
-                <p>- abc -</p>
-             </div>
-            <div className='card'>
-                <h3> - Zaynah- </h3>
-                <img src = {zaynahAvatar}  alt = "cartoon rendition of Zaynah"/>
-                <span className='team-member'> </span>
-                <p className='person'> Zaynah</p>
-                <p>- abc -</p>
+                <p>Fatal flaw: none, just good vibes</p>
                 <p>- abc -</p>
                 <p>- abc -</p>
                 <p>- abc -</p>
              </div>
+           
             <div className='card'>
                 <h3> - Subrina- </h3>
+                <img src = {subrinaAvatar}  alt = "cartoon rendition of Subrina"/>
                 <span className='team-member'> </span>
                 <p className='person'> Subrina</p>
-                <p>- abc -</p>
+                <p>Fatal flaw: underestimates herself</p>
                 <p>- abc -</p>
                 <p>- abc -</p>
                 <p>- abc -</p>
