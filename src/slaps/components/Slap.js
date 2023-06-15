ea8a2c50bb376ffbd90610330be4ebe1e360236c
Nasthasia Usoh <nasthasia@hotmail.co.uk>
2023-06-15 18:56:16 +0100

    fixing merge conflicts

--- src/slaps/components/Slap.js
+++ src/slaps/components/Slap.js
@@ -31,30 +31,3 @@ const Slap = ({ slap, loggedInUser }) => {
   );
 };
 export default Slap
-
-// const Slap = ({slap}) => {
-
-//   return (
-//     <div className='timeline-list-of-slaps'>
-//      <div className='timeline-slap-component'>
-//         <div className='timeline-user-profile-details'>
-//           <Link to={`/profile/${slap.user.id}`}>
-//             <img src={slap.user.profilePicture} width={170} />
-//           </Link>
-//            <div className='timeline-user-slap-contents'>
-
-//             <Link to={`/profile/${slap.user.id}`}>
-//               <p><b>{slap.user.username}</b></p>
-//             </Link>
-
-//             <p>{slap.message}</p>
-//             <p><b>Mood:</b> {slap.mood}</p>
-//             <p><b>Time Posted:</b> {slap.dateTime}</p>
-//            </div>
-//         </div>
-//     </div>
-//     </div>
-//   )
-// }
-
-// export default Slap
