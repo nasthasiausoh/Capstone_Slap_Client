6023f9a5fb451c25e5321ff97f9b14b37bf5b4d4
Zaynah <zaynah.sadiq@hotmail.com>
2023-06-15 11:50:27 +0100

    added styling

--- src/aboutUs/components/AboutUs.js
+++ src/aboutUs/components/AboutUs.js
@@ -10,45 +10,40 @@ const AboutUs = () => {
     <div className='about-us'>
         <div className='card-container'>
             <div className='card'>
-                <h3> - Natasha-</h3>
+            <h3> - Keyboard Warrior 1 - </h3>
                 <img src = {natashaAvatar}  alt = "cartoon rendition of Natasha"/>
                 <span className='team-member'> </span>
                 <p className='person'> Natasha</p>
-                <p>Fatal flaw: works too much</p>
-                <p>- abc -</p>
-                <p>- abc -</p>
-                <p>- abc -</p>
+                <p>Unofficial ambassador of going the extra mile, the ultimate MVP</p>
+                <p id='flaw'>Fatal flaw: works too much</p>
+          
              </div>
              <div className='card'>
-                <h3> - Zaynah- </h3>
+                <h3> - Keyboard Warrior 2 - </h3>
                 <img src = {zaynahAvatar}  alt = "cartoon rendition of Zaynah"/>
                 <span className='team-member'> </span>
                 <p className='person'> Zaynah</p>
-                <p>Fatal flaw: has Hayfever</p>
-                <p>- abc -</p>
-                <p>- abc -</p>
-                <p>- abc -</p>
+                <p>Never runs out of things to say, a black belt in conversation</p>
+                <p id='flaw'>Fatal flaw: has Hayfever</p>
+  
              </div>
             <div className='card'>
-                <h3> - Rada- </h3>
+            <h3> - Keyboard Warrior 3 -</h3>
                 <img src = {radaAvatar}  alt = "cartoon rendition of Rada"/>
                 <span className='team-member'> </span>
                 <p className='person'> Rada</p>
-                <p>Fatal flaw: none, just good vibes</p>
-                <p>- abc -</p>
-                <p>- abc -</p>
-                <p>- abc -</p>
+                <p>If sunshine was a person, it would be Rada</p>
+                <p id='flaw'>Fatal flaw: none, just good vibes</p>
              </div>
            
             <div className='card'>
-                <h3> - Subrina- </h3>
+            <h3>- Keyboard Warrior 4 - </h3>
                 <img src = {subrinaAvatar}  alt = "cartoon rendition of Subrina"/>
                 <span className='team-member'> </span>
                 <p className='person'> Subrina</p>
-                <p>Fatal flaw: underestimates herself</p>
-                <p>- abc -</p>
-                <p>- abc -</p>
-                <p>- abc -</p>
+                <p> From internet breaker to internet creator, a real life cheat-code</p>
+                <p id='flaw'>Fatal flaw: underestimates herself</p>
+          
              </div>
 
         </div>
