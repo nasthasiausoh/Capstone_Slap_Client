79ff327007f760ce8eef1f6ab4d7a3735e063f03
Rada Kanchananupradit <rada.kanchan@gmail.com>
2023-06-15 18:45:44 +0100

    fixing merge conflicts

--- src/slaps/components/SlapForm.js
+++ src/slaps/components/SlapForm.js
@@ -1,28 +1,18 @@
 import { useState } from "react";
 import Picker from "@emoji-mart/react";
 import data from "@emoji-mart/data";
+import "../SlapFormStyles.css";
 
 
 const SlapForm = ({loggedInUser, addNewSlap}) => {
 
-  const[message, setMessage]= useState('');
-  const [mood, setMood] = useState('');
-  const [showModal, setShowModal] = useState(false);
-
-  const handleOpenModal = () => {
-      setShowModal(true);
-  };
-  
-  const handleCloseModal = () => {
-    setShowModal(false);
-    setMessage('');
-    setMood('');
-  };
+  const [message, setMessage]= useState('');
+  const [showPicker, setShowPicker] = useState(false);
+  let [currentEmoji, setCurrentEmoji] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     handleAddSlap();
-    handleCloseModal();
   };
 
   const handleAddSlap = async () => {
@@ -34,57 +24,43 @@ const SlapForm = ({loggedInUser, addNewSlap}) => {
     addNewSlap(slapInfo)
   };
 
-  const [isPickerVisible, setPickerVisible] = useState(false);
-  const [currentEmoji, setCurrentEmoji] = useState("");
-
-  // const handleTogglePicker = () => setPickerVisible(!isPickerVisible);
-
-  // const handleEmojiClick = (emojiObject) => {
-  // setCurrentEmoji(emojiObject.emoji);
-  // setPickerVisible(false);
-  // };
-
-
+  const handleEmojiToggle = () => {
+    setShowPicker(!showPicker);
+  }
 
       
   return (
-    <div>
-      <button onClick={handleOpenModal}>Add a Slap!</button>
-      {showModal && (
+    <div className="slap-form-component">
+      <h2>Create a Slap!</h2>
         <div>
           <form onSubmit={handleSubmit}>
-          <input
-                type="text"
-                value={message}
-                onChange={(event) => setMessage(event.target.value)}
-              />
-          <input
-                type="text"
-                value={currentEmoji.native}
-                onChange={(event) => setMood(event.target.value)}
-              />
-          <button type="submit">Submit</button>
+            <input
+              type="text"
+              placeholder="Enter your message"
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+            />
+
+            <input
+              type="text"
+              placeholder="Select an Emoji"
+              value={currentEmoji.native}
+              onClick={handleEmojiToggle}
+            />
+
+            {showPicker ? <Picker
+                className="picker"
+                data ={data}
+                previewPosition="none"
+                onEmojiSelect={(e) => {
+                setCurrentEmoji(e)
+                handleEmojiToggle();
+              }}/> : ""}
+
+          <button type="submit" className="slap-submit-button">Submit</button>
 
           </form>
-
-          <div className='d-flex flex-column align-items-center'>
-            <h1 className='mt-5 mb-5'>{currentEmoji.native || 'select emoji' }</h1>
-            <button className='btn btn-primary' onClick={() => setPickerVisible(!isPickerVisible)}> open emoji picker</button>
-            <div className={isPickerVisible ? 'd-block' : 'd-none'}>
-              <Picker
-              data ={data}
-              previewPosition="none"
-              onEmojiSelect={(e) => {
-              setCurrentEmoji(e)
-              setPickerVisible(!isPickerVisible);
-              }}/>
-            </div>
-
-          </div> 
-
-          <button onClick={handleCloseModal}>Exit</button>
         </div>
-      )};
     </div>
   );
 
